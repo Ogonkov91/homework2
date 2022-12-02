bc6a36969de4b5e5b5e69203b33b8c0e497441fc
Ogonkov91 <118393581+Ogonkov91@users.noreply.github.com>
2022-12-02 12:37:46 +0300

    add something

--- task1/Program.cs
+++ task1/Program.cs
@@ -9,9 +9,6 @@ int num;
                 Console.Write(ii + " Кажется это не трёхзначное число, попробуем ещё раз: ");
                 num = Convert.ToInt32(Console.ReadLine());
             }
-            if (num <= 100 | num > 999)
-            {
-                Console.WriteLine("Чтото пошло не так, программа прервана: ");
-            }
+            
             num = num / 10 % 10;
             Console.WriteLine($"Вторая цифра введённого числа: {num}");
