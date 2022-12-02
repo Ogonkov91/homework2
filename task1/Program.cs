934d57dc7321c498bb602e9f208edf4e93e1ba25
Ogonkov91 <ogonkov.91@mail.rum>
2022-12-02 11:01:06 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿ int num;
+ Console.Write("Введите трёхзначное число: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            // поиграемся с For и логическими операторами. Проверка числа и выход при 3-ей ошибке
+            for (int ii = 1; ii < 3 && (num <= 100 | num > 999); ii++)
+            {
+                Console.Write(ii + " Кажется это не трёхзначное число, попробуем ещё раз: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            if (num <= 100 | num > 999)
+            {
+                Console.WriteLine("Чтото пошло не так, программа прервана: ");
+            }
+            num = num / 10 % 10;
+            Console.WriteLine($"Вторая цифра введённого числа: {num}");
---
+++ task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+           int num, count, numDigit; 
+
+            Console.Write("Введите любое число: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            numDigit = NumberDigit(num);// Пробуем функцию/метод. (прим вероятно подошло бы и для для проверки первой задачи)
+
+           int NumberDigit(int number)
+{
+    count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}         
+
+            if (numDigit <= 2)
+            {
+                Console.WriteLine("третьей цифры нет");
+            }
+            else
+            {
+                if (numDigit > 3)
+                {
+                    num = num / Convert.ToInt32(Math.Pow(10, numDigit - 3)); // немного Math
+                }
+                num = num % 10;
+                Console.WriteLine($"Третья цифра введённого числа: {num}");
+            }
+    
